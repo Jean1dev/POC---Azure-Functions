720c7b6c7b83bcf173b882503865b5199b05584e
Jean <jeanluca.fernandes@totvs.com.br>
2020-11-20 11:34:43 -0200

    criado projeto com 1° function

---
+++ first-function/index.js
@@ -0,0 +1,38 @@
+const { DotenvAzure } = require('dotenv-azure')
+const mongoClient = require('mongodb')
+const axios = require('axios')
+
+module.exports = async (context, req) => {
+    await new DotenvAzure().config()
+    
+    const item = await getItem()
+    const result = await insertItem(item)
+    context.res = {
+        // status: 200, /* Defaults to 200 */
+        body: result.insertedId || 'Ocorreu um erro'
+    };
+}
+
+
+async function getItem() {
+    try {
+        const { data } = await axios.post(process.env.LOGIC_APP_URL, {
+            "codigo-item-datasul": "'A2"
+        })
+
+        return data
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+async function insertItem(item) {
+    const client = await mongoClient.connect(process.env.MONGO_URL, { useNewUrlParser: true })
+        .catch(err => console.log('ERRO DE CONEXAO COM O MONGO'))
+
+    if (!client) return null
+
+    const db = client.db('testedb')
+
+    return await db.collection('itens').insertOne(item)
+}
