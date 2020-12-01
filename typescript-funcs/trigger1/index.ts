0848dd48685c0f93531b2a0eafe37986633ed5bf
Jean <jeanluca.fernandes@totvs.com.br>
2020-12-01 16:27:56 -0200

    criado funcao

---
+++ typescript-funcs/trigger1/index.ts
@@ -0,0 +1,21 @@
+import { AzureFunction, Context, HttpRequest } from "@azure/functions"
+import JSFunc from './teste'
+
+const httpTrigger: AzureFunction = async function (context: Context, req: HttpRequest): Promise<void> {
+    context.log('HTTP trigger function processed a request.');
+    const name = (req.query.name || (req.body && req.body.name));
+    const responseMessage = name
+        ? "Hello, " + name + ". This HTTP triggered function executed successfully."
+        : "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.";
+
+    const opa = req.query?.name
+    context.log('valor do opa', opa)
+    JSFunc()
+    context.res = {
+        // status: 200, /* Defaults to 200 */
+        body: responseMessage
+    };
+
+};
+
+export default httpTrigger;
